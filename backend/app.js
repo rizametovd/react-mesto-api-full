6520ef51cfeed5d74b8f76a30a3117eb450f4d2c
David <rizametov@gmail.com>
2021-07-01 15:42:13 +0300

    Поправил порт

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
